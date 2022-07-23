bf3e7c5f25a8ea0cfac0e0bca233a05de636de0a
HugoMichard <hugomichard@yahoo.com>
2022-07-23 12:34:31 +0200

    initial commit

---
+++ js/pond/fish_pond.js
@@ -0,0 +1,225 @@
+const Fish = require('./fish');
+const Target = require('./target');
+const water = require('./watermodel.js')
+
+
+class FishPond {
+  constructor(window){
+    this.var=0.001;
+    this.maxFood=30;
+    this.maxFish=30;
+    this.window=window;
+    this.height = this.window.innerHeight;
+    this.width = this.window.innerWidth;
+    this.vh = this.height/100;
+    this.vw = this.width/100;
+    this.c=0;
+    //this.base_image = new Image();
+    //this.base_image.src = '/home/hugo/Perso/PProjects/HugoWebSite2/imgs/texture2.png';
+    //this.texture_image = new Image();
+    //this.texture_image.src = '/home/hugo/Perso/PProjects/HugoWebSite2/imgs/texture3.jpg';
+
+    this.spots=[new Target(0,0,0)];
+    for(let i = 1;i < 100;i++)
+      this.spots.push(new Target(0,0,0,this.spots[i-1]));
+    this.spots[0].nextSpot=this.spots[this.spots.length - 1];
+
+    const halfh=this.height/2;
+    const halfw=this.width/2;
+    for(let i = 0;i < Math.floor(this.spots.length) / 2;i++){
+      this.spots[i].x = halfw + ( Math.random() * halfw)*Math.cos(i);
+      this.spots[i].y = halfh + ( Math.random() * halfh)*Math.sin(i);
+    }
+    for(let i = Math.floor(this.spots.length / 2);i < this.spots.length;i++){
+      this.spots[i].x = halfw + (halfw/4 + Math.random() * halfw/1.5)*Math.cos(-i);
+      this.spots[i].y = halfh + (halfh/3 + Math.random() * halfh/2)*Math.sin(-i);
+    }
+
+    this.foods=[];
+    this.fish=[];
+    let fishCount = 5;
+    this.ripples=[];
+
+    this.numberOfFish = fishCount;
+    for(let i = 0;i < fishCount;i++)
+      this.addFish();
+  }
+
+  start(canvas){
+    const h = this.height;
+    const w = this.width;
+    this.height = this.window.innerHeight - 20;
+    this.width = this.window.innerWidth - 20;
+    canvas.height = this.window.innerHeight - 20;
+    canvas.width = this.window.innerWidth - 20;
+
+    console.log("INITIALIZING WATER MODEL")
+    var waterModel = new water.WaterModel(this.width, this.height, {
+			resolution:2.0,
+			interpolate:false,
+			damping:0.985,
+			clipping:5,
+			evolveThreshold:0.05,
+			maxFps:30,
+			showStats:true
+		});
+    console.log("INITIALIZING WATER CANVAS")
+
+    this.watercanvas = new water.WaterCanvas(this, waterModel, {
+      lightRefraction:9.0,
+			lightReflection:0.1,
+			maxFps:20,
+			showStats:true
+    });
+    console.log("INITIALIZING RAIN DROPS")
+
+    var raindrop 	= water.create2DArray(water.createRadialCanvas(4,4));
+    var rainMaker = new water.RainMaker(this.width, this.height, waterModel, raindrop);
+    rainMaker.setRaindropsPerSecond(1);
+    water.enableMouseInteraction(waterModel);
+
+    const ctx = canvas.getContext("2d");
+
+    const startAnimation = () => {
+      this.vh = this.height/100;
+      this.vw = this.width/100;
+
+      if(w !== this.width || h !== this.height){
+        const halfh=this.height/2;
+        const halfw=this.width/2;
+        for(let i = 0;i < Math.floor(this.spots.length) / 2;i++){
+          this.spots[i].x = halfw + ( Math.random() * halfw)*Math.cos(i);
+          this.spots[i].y = halfh + ( Math.random() * halfh)*Math.sin(i);
+        }
+        for(let i = Math.floor(this.spots.length / 2);i < this.spots.length;i++){
+          this.spots[i].x = halfw + (halfw/4 + Math.random() * halfw/1.5)*Math.cos(-i);
+          this.spots[i].y = halfh + (halfh/3 + Math.random() * halfh/2)*Math.sin(-i);
+        }
+      }
+
+      this.render(ctx);
+
+      setTimeout(startAnimation, 1000/30);
+    };
+    const addExtraFish = () => {
+        if(this.numberOfFish < this.maxFish) {
+            this.addFish();
+            this.numberOfFish += 1;
+        }
+        setTimeout(addExtraFish, 1000 * 10);
+    }
+
+    startAnimation();
+    addExtraFish();
+  }
+
+  render(ctx){
+    ctx.globalAlpha = 1;
+    ctx.fillStyle='#66ccff';
+    ctx.fillRect(0, 0,this.width,this.height);
+    ctx.globalAlpha = 0.7;
+    //ctx.drawImage(this.base_image, 0, 0, this.width, this.height);
+    ctx.globalAlpha = 0.3;
+    //ctx.drawImage(this.texture_image, 0, 0, this.width, this.height);
+    ctx.globalAlpha = 0.9;
+
+    this.fish.sort((a,b)=>b.mass-a.mass);
+    for(let i = 0;i < this.fish.length;i++)
+      this.fish[i].render(ctx);
+    for(let i = 0;i < this.foods.length;i++)
+      this.foods[i].render(ctx);
+    this.watercanvas.drawNextFrame();
+
+    this.fontSize = this.vh*10;
+    if(this.width/20 < this.fontSize)
+      this.fontSize = this.vw*5;
+  }
+  click(x,y){
+    let food = new Target(x,y,3);
+    if(
+        x > 0 &&
+        y > this.height-this.vh*4-this.fontSize * 0.5 &&
+        x < this.vh*4+this.textWidth &&
+        y < (this.height-this.vh*4-this.fontSize * 0.5) + this.fontSize*0.5+4*this.vh
+    ){
+      this.addFish();
+      this.var+=0.001
+    }
+    else{
+      if(this.foods.length < this.maxFood)
+        this.foods.push(food);
+      else{
+        this.foods[0].value=-1;
+        this.foods.shift();
+        this.foods.push(food);
+      }
+      for(let i = 0;i < this.fish.length;i++){
+        this.fish[i].foodNotify(food);
+      }
+    }
+  }
+  addFish(){
+    let hov = Math.random() * 2;
+    let x,y,dir;
+    if(hov > 1){
+      y = this.height / 2;
+      hov = Math.random() * 2;
+      if(hov > 1){
+        x = -50;
+        dir = 0.0001;
+      }
+      else{
+        x = 50 + this.width;
+        dir = Math.PI;
+      }
+    }
+    else{
+      hov = Math.random() * 2;
+      x=this.width / 2
+      if(hov > 1){
+        y = -100;
+        dir = Math.PI / 2;
+      }
+      else{
+        y = this.height + 100;
+        dir = Math.PI / 2 * 3;
+      }
+    }
+    this.fish.push(
+      new Fish({
+        mass:35+Math.sqrt(Math.random()*10000)+this.var,
+        x:x,
+        y:y,
+        pond:this,
+        direction:dir
+      })
+    );
+  }
+  getClosestFood(x,y){
+    if(this.foods.length < 1)
+      return null;
+    let target = this.foods[0]
+    for(let i = 1;i < this.foods.length;i++)
+      if(this.foods[i].getDistance(x,y) < target.getDistance(x,y))
+        target=this.foods[i];
+    return target;
+  }
+  getSpot(){
+    return this.spots[Math.floor(this.spots.length * Math.random())];
+  }
+  bite(x,y,radius,fish){
+    for(let i = 0;i < this.foods.length;i++){
+      if(this.foods[i].getDistance(x,y) < radius + 10){
+        this.foods[i].eaten(fish);
+        this.foods.splice(i,1);
+        i--;
+      }
+    }
+    if(fish.target && fish.target.value===0)
+      for(let i = 0;i < this.spots.length;i++)
+        if(this.spots[i].getDistance(x,y) < 200)
+          this.spots[i].eaten(fish);
+  }
+}
+
+module.exports = FishPond;
---
+++ js/pond/watermodel.js
@@ -0,0 +1,490 @@
+class WaterCanvas {
+    constructor(pond, waterModel, props) {
+
+        // If a certain property is not set, use a default value
+        props = props || {};
+        this.lightRefraction 	= props.lightRefraction 	|| 9.0;
+        this.lightReflection	= props.lightReflection 	|| 0.1;
+        this.showStats			= props.showStats 			|| false;
+
+        this.pond = pond;
+        this.waterModel = waterModel;
+        
+        this.canvas = document.getElementById('pond');  
+        this.canvasHelp = document.getElementById('pond');
+
+        if(!this.canvas.getContext || !this.canvasHelp.getContext){ 
+            alert("You need a browser that supports the HTML5 canvas tag.");
+            return; // No point to continue
+        }
+
+        this.ctx = this.canvas.getContext('2d');  
+        this.ctxHelp = this.canvasHelp.getContext('2d');  		
+
+        // Find out the FPS at certain intervals
+        this.fps = -1;
+        if(this.showStats){	
+            this.fpsCounter = 0;
+            this.prevMs = 0;
+                
+            var self = this; 
+            setInterval(function(){
+                self.findFps();
+            }, 1000);
+        }
+
+    }
+    drawNextFrame() {
+        if(!this.waterModel.isEvolving()){
+            // Wait some time and try again
+            var self = this; 
+            
+            // Nothing else to do for now
+            return;
+        }
+                
+        // Make the canvas give us a CanvasDataArray. 
+        // Creating an array ourselves is slow!!!
+        // https://developer.mozilla.org/en/HTML/Canvas/Pixel_manipulation_with_canvas
+
+		this.pixelsIn = this.ctx.getImageData(0, 0, this.pond.width, this.pond.height).data;
+        var imgDataOut = this.ctx.getImageData(0, 0, this.pond.width, this.pond.height);
+        var pixelsOut = imgDataOut.data;
+        for (var i = 0; n = pixelsOut.length, i < n; i += 4) {
+            var pixel = i/4;
+            var x = pixel % this.pond.width;
+            var y = (pixel-x) / this.pond.width;
+            
+            var strength = this.waterModel.getWater(x,y);
+            // Refraction of light in water
+
+			var refraction = Math.round(strength * this.lightRefraction);
+
+			var xPix = x + refraction;
+            var yPix = y + refraction;     
+            
+            if(xPix < 0) xPix = 0;
+            if(yPix < 0) yPix = 0;
+            if(xPix > this.pond.width-1) xPix = this.pond.width-1;
+            if(yPix > this.pond.height-1) yPix = this.pond.height-1;			
+
+			// Get the pixel from input
+            var iPix = ((yPix * this.pond.width) + xPix) * 4;
+
+			var red 	= this.pixelsIn[iPix  ];
+            var green 	= this.pixelsIn[iPix+1];
+            var blue 	= this.pixelsIn[iPix+2];
+
+			// Set the pixel to output
+            strength *= this.lightReflection;
+            strength += 1.0;
+    
+            pixelsOut[i  ] = red *= strength;
+            pixelsOut[i+1] = green *= strength;
+            pixelsOut[i+2] = blue *= strength;
+            pixelsOut[i+3] = 255; // alpha 
+        }
+    
+        this.ctx.putImageData(imgDataOut, 0,0);
+
+        // Make the browser call this function at a new render frame
+        var self = this; // For referencing 'this' in internal eventListeners
+                
+    }
+    findFps(){
+        if(!this.showStats)
+            return;
+        
+        var nowMs = new Date().getTime();
+        var diffMs = nowMs - this.prevMs;
+    
+        this.fps = Math.round( ((this.fpsCounter*1000) / diffMs) * 10.0 ) / 10.0;
+        
+        this.prevMs = nowMs;
+        this.fpsCounter = 0;
+    }
+    getFps(){
+        return this.fps;
+    }
+    setLightRefraction(lightRefraction){
+        this.lightRefraction = lightRefraction;	
+    }
+    setLightReflection(lightReflection){
+        this.lightReflection = lightReflection;	
+    }
+
+}
+
+class WaterModel {
+	constructor(width, height, props) {
+		// If a certain property is not set, use a default value
+		props = props || {};
+		this.resolution 		= props.resolution 		|| 2.0;
+		this.interpolate 		= props.interpolate 	|| false;
+		this.damping 			= props.damping 		|| 0.985;
+		this.clipping 			= props.clipping 		|| 5;	
+		this.maxFps 			= props.maxFps 			|| 30;
+		this.showStats 			= props.showStats 		|| false;
+		this.evolveThreshold 	= props.evolveThreshold	|| 0.05;
+
+		this.width = Math.ceil(width/this.resolution);
+		this.height = Math.ceil(height/this.resolution);
+
+		this.resetSizeAndResolution(width, height, this.resolution)
+
+		// Create water model 2D arrays
+		this.swapMap;
+
+		this.setMaxFps(this.maxFps);
+
+		this.evolving = false; // Holds whether it's needed to render frames
+
+		// Find out the FPS at certain intervals
+		this.fps = -1;
+		if(this.showStats){	
+			this.fpsCounter = 0;
+			this.prevMs = 0;
+				
+			var self = this; 
+			setInterval(function(){
+				self.findFps();
+			}, 1000);
+		}
+	}
+
+	getWater(x, y) {
+		let xTrans = x/this.resolution;
+		let yTrans = y/this.resolution;
+		if(!this.interpolate || this.resolution==1.0){
+			let xF = Math.floor(xTrans); 
+			let yF = Math.floor(yTrans);	
+	
+			if(xF>this.width-1 || yF>this.height-1)
+				return 0.0;
+
+			return this.depthMap1[xF][yF];
+		}
+		
+		
+		// Else use Bilinear Interpolation
+		let xF = Math.floor(xTrans); 
+		let yF = Math.floor(yTrans);
+		let xC = Math.ceil(xTrans); 
+		let yC = Math.ceil(yTrans);	
+	
+		if(xC>this.width-1 || yC>this.height-1)
+			return 0.0;
+
+		// Now get 4 points from the array
+		var br = this.depthMap1[xF][yF];
+		var bl = this.depthMap1[xC][yF];
+		var tr = this.depthMap1[xF][yC];
+		var tl = this.depthMap1[xC][yC];
+		
+		// http://tech-algorithm.com/articles/bilinear-image-scaling/
+		//	D   C
+		//	  Y
+		//	B	A
+		// Y = A(1-w)(1-h) + B(w)(1-h) + C(h)(1-w) + Dwh
+	
+		var xChange = xC - xTrans;
+		var yChange = yC - yTrans;
+		var intpVal =
+				tl*(1-xChange)	*	(1-yChange) +  
+				tr*(xChange)	*	(1-yChange) +
+				bl*(yChange)	*	(1-xChange) +  
+				br*xChange		*	yChange;
+	
+		return intpVal;
+	}
+
+	setInterpolation(interpolate) {
+		this.interpolate = interpolate;
+	}
+
+	touchWater(x, y, pressure, array2d) {
+		this.evolving = true;
+
+		x = Math.floor(x/this.resolution); 
+		y = Math.floor(y/this.resolution);
+		
+		// Place the array2d in the center of the mouse position
+		if(array2d.length>4 || array2d[0].length>4){
+			x-=array2d.length/2;
+			y-=array2d[0].length/2;
+		}
+		
+		if(x<0) x = 0;
+		if(y<0) y = 0;
+		if(x>this.width) x = this.width;
+		if(y>this.height) y = this.height;
+	
+		// Big pixel block
+		for(var i = 0; i < array2d.length; i++){
+			for(var j = 0; j < array2d[0].length; j++){
+	
+				if(x+i>=0 && y+j>=0 && x+i<=this.width-1 && y+j<=this.height-1) {
+					this.depthMap1[x+i][y+j] -= array2d[i][j] * pressure;
+				}
+				
+			}
+		}
+	}
+
+	renderNextFrame() {
+		if(!this.evolving)
+			return;
+
+		this.evolving = false;
+		
+		for (var x = 0; x < this.width; x++) {
+			for (var y = 0; y < this.height; y++) {
+
+				// Handle borders correctly
+				var val = 	(x==0 				? 0 : this.depthMap1[x - 1][y]) +
+							(x==this.width-1 	? 0 : this.depthMap1[x + 1][y]) +
+							(y==0 				? 0 : this.depthMap1[x][y - 1]) +
+							(y==this.height-1 	? 0 : this.depthMap1[x][y + 1]);
+
+				// Damping
+				val = ((val / 2.0) - this.depthMap2[x][y]) * this.damping;
+				
+				// Clipping prevention
+				if(val>this.clipping) val = this.clipping;
+				if(val<-this.clipping) val = -this.clipping;
+				
+				// Evolve check
+				if(Math.abs(val)>this.evolveThreshold) 
+					this.evolving = true; 
+
+				
+				this.depthMap2[x][y] = val;
+			}
+		}
+
+		// Swap buffer references
+		this.swapMap 	= this.depthMap1;
+		this.depthMap1 	= this.depthMap2;
+		this.depthMap2 	= this.swapMap;
+		
+		this.fpsCounter++;
+	}
+
+	isEvolving() {
+		return this.evolving;
+	}
+
+	findFps() {
+		if(!this.showStats)
+			return;
+	
+		var nowMs = new Date().getTime();
+		var diffMs = nowMs - this.prevMs;
+
+		this.fps = Math.round( ((this.fpsCounter*1000) / diffMs) * 10.0 ) / 10.0;
+		
+		this.prevMs = nowMs;
+		this.fpsCounter = 0;
+	}
+	getFps() {
+		return this.fps
+	}
+	setMaxFps(maxFps) {
+		this.maxFps = maxFps;
+	
+		clearInterval(this.maxFpsInterval);
+	
+		// Updating of the animation
+		var self = this; // For referencing 'this' in internal eventListeners	
+		
+		if(this.maxFps>0){
+			this.maxFpsInterval = setInterval(function(){
+				self.renderNextFrame();
+			}, 1000/this.maxFps); 	
+		}
+	}
+	setDamping(damping) {
+		this.damping = damping
+	}
+	resetSizeAndResolution(width, height, resolution) {
+		this.width = Math.ceil(width/resolution);
+		this.height = Math.ceil(height/resolution);
+		this.resolution = resolution;
+		
+		this.depthMap1 = new Array(this.width); 
+		this.depthMap2 = new Array(this.width);
+		for(var x = 0; x < this.width; x++){
+			this.depthMap1[x] = new Array(this.height);
+			this.depthMap2[x] = new Array(this.height);
+			
+			for (var y = 0; y < this.height; y++) {
+				this.depthMap1[x][y] = 0.0;
+				this.depthMap2[x][y] = 0.0;
+			}
+		}
+	}
+
+	resetSizeAndResolution(width, height, resolution){
+		this.width = Math.ceil(width/resolution);
+		this.height = Math.ceil(height/resolution);
+		this.resolution = resolution;
+		
+		this.depthMap1 = new Array(this.width); 
+		this.depthMap2 = new Array(this.width);
+		for(var x = 0; x < this.width; x++){
+			this.depthMap1[x] = new Array(this.height);
+			this.depthMap2[x] = new Array(this.height);
+			
+			for (var y = 0; y < this.height; y++) {
+				this.depthMap1[x][y] = 0.0;
+				this.depthMap2[x][y] = 0.0;
+			}
+		}
+	}
+}
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+//                                 Util functions                             //
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * A class to mimic rain on the given waterModel with raindrop2dArray's as raindrops.
+ */
+class RainMaker {
+	constructor(width, height, waterModel, raindrop2dArray) {
+		this.width = width;
+		this.height = height;
+		this.waterModel = waterModel;
+		this.raindrop2dArray = raindrop2dArray;
+	
+		this.rainMinPressure = 1;
+		this.rainMaxPressure = 3;
+	}
+	raindrop() {
+		var x = Math.floor(Math.random() * this.width);
+		var y = Math.floor(Math.random() * this.height);
+		this.waterModel.touchWater(x, y, this.rainMinPressure + Math.random() * this.rainMaxPressure, this.raindrop2dArray);
+	}
+	setRaindropsPerSecond(rps) {
+		this.rps = rps;
+	
+		clearInterval(this.rainInterval);
+	
+		if(this.rps>0) {
+			var self = this; 
+			this.rainInterval = setInterval(function(){
+				self.raindrop();
+			}, 1000/this.rps); 	
+		}
+	}
+
+	setRainMinPressure(rainMinPressure){
+		this.rainMinPressure = rainMinPressure;
+	}
+
+	setRainMaxPressure(rainMaxPressure){
+		this.rainMaxPressure = rainMaxPressure;
+	}
+}	
+
+
+/**
+ * Enables mouse interactivity by adding event listeners to the given documentElement and
+ * using the mouse coordinates to 'touch' the water.
+ */
+function enableMouseInteraction(waterModel){		
+	var mouseDown = false;
+	
+	var canvasHolder = document.getElementById("pond");
+	
+	canvasHolder.addEventListener("mousedown", function(e){
+		mouseDown = true;
+		var x = (e.clientX - canvasHolder.offsetLeft) + document.body.scrollLeft + document.documentElement.scrollLeft;
+		var y = (e.clientY - canvasHolder.offsetTop) + document.body.scrollTop + document.documentElement.scrollTop;
+		waterModel.touchWater(x, y, 1.5, mouseDown ? finger : pixel);
+	}, false);
+	
+	canvasHolder.addEventListener("mouseup", function(e){
+		mouseDown = false;
+	}, false);
+	
+	canvasHolder.addEventListener("mousemove", function(e){
+		var x = (e.clientX - canvasHolder.offsetLeft) + document.body.scrollLeft + document.documentElement.scrollLeft;
+		var y = (e.clientY - canvasHolder.offsetTop) + document.body.scrollTop + document.documentElement.scrollTop;
+		// mozPressure: https://developer.mozilla.org/en/DOM/Event/UIEvent/MouseEvent
+		waterModel.touchWater(x, y, 1.5, mouseDown ? finger : pixel);
+	}, false);
+}
+
+/**
+ * Creates a canvas with a radial gradient from white in the center to black on the outside.
+ */
+function createRadialCanvas(width, height){
+	// Create a canvas
+	var pointerCanvas = document.createElement('canvas');  
+	pointerCanvas.setAttribute('width', width);  
+	pointerCanvas.setAttribute('height', height);  
+	pointerCtx = pointerCanvas.getContext('2d'); 
+	
+	// Create a drawing on the canvas
+	var radgrad = pointerCtx.createRadialGradient(width/2,height/2,0,  width/2,height/2,height/2);
+	radgrad.addColorStop(0, '#fff');
+	radgrad.addColorStop(1, '#000');
+
+	pointerCtx.fillStyle = radgrad;
+	pointerCtx.fillRect(0,0,width,height);	
+	
+	return pointerCanvas;
+}
+
+/**	
+ * Creates a 2D pointer array from a given canvas with a grayscale image on it. 
+ * This canvas image is then converted to a 2D array with values between -1.0 and 0.0.
+ * 
+ * Example:
+ * 	var array2d = [
+ * 		[0.5, 1.0, 0.5], 
+ * 		[1.0, 1.0, 1.0], 
+ * 		[0.5, 1.0, 0.5]
+ * 	];
+ */
+function create2DArray(canvas){
+	var width = canvas.width;
+	var height = canvas.height;
+
+	// Create an empty 2D  array
+	var pointerArray = new Array(width); 
+	for(var x = 0; x < width; x++){
+		pointerArray[x] = new Array(height);
+		for (var y = 0; y < height; y++) {
+			pointerArray[x][y] = 0.0;
+		}
+	}
+
+	// Convert gray scale canvas to 2D array
+	var pointerCtx = canvas.getContext('2d'); 
+	var imgData = pointerCtx.getImageData(0, 0, width, height);
+	var pixels = imgData.data;	
+
+	for (var i = 0; n = pixels.length, i < n; i += 4) {				
+		// Get the pixel from input
+		var pixVal 	= pixels[i];// only use red
+		var arrVal = pixVal/255.0;
+
+		var pixel = i/4;
+		var x = pixel % width;
+		var y = (pixel-x) / width;
+
+		pointerArray[x][y] = arrVal;
+	}				
+	
+	return pointerArray;			
+}
+
+module.exports = { create2DArray, createRadialCanvas, RainMaker, WaterCanvas, WaterModel, enableMouseInteraction }
+
+const pixel = create2DArray(createRadialCanvas(2,2));
+const finger = create2DArray(createRadialCanvas(14,14));
